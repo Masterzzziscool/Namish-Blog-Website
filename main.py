8e0493e6763f8d8d5504bbc784fd58097ab0dba4
Namish <namishjohar14@gmail.com>
2023-07-10 18:59:57 +0530

    Add psycopg2-binary to requirements.txt

--- main.py
+++ main.py
@@ -82,8 +82,8 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
 
 
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
